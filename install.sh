d01d1b3da2cc1baeac469640c00bc94b9e5844cc
RmnJL <44286466+RmnJL@users.noreply.github.com>
2024-02-17 23:54:47 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ yum install gcc-c++ libpcap-devel.x86_64 libpcap.x86_64 "ncurses*"
 fi
 sudo wget -O /root/nethogs.zip https://github.com/RmnJL/Net/archive/refs/heads/master.zip
 unzip /root/nethogs.zip
-mv -f /root/Nethogs-Json-main-master /root/nethogs
+mv -f /root/Net-master /root/nethogs
 cd /root/nethogs/
 chmod 744 /root/nethogs/determineVersion.sh
 sudo make install
